d5a49c3dde352c74a55c431af3effb3770ad9cfe
Adrian <adrian.zigerlig@ost.ch>
2022-05-10 20:37:19 +0200

    REC-96 Added Recipe model and RecipeForm to visualize default Recipe and send default Post method

--- Code/recipemanager/src/components/pages/RecipeForm.tsx
+++ Code/recipemanager/src/components/pages/RecipeForm.tsx
@@ -1,6 +1,48 @@
 import { Component } from "react";
+import { Navigate } from 'react-router-dom'
+import { createNewRecipe, RecipesUrl } from "../../models/Recipe";
 import './RecipeForm.css'
 
 export class RecipeForm extends Component {
+    state = {
+        redirect: false,
+        recipe: createNewRecipe(),
+        error: ''
+    }
 
+    save = async () => {
+        const response = await fetch(`${RecipesUrl}`, {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify(this.state.recipe)
+        })
+
+        if (response.status >= 300) {
+            this.setState({ error: 'Error occurred!', loading: false })
+        } else {
+            const update = await response.json()
+            const recipe = Object.assign(this.state.recipe, update)
+            this.setState({ redirect: true, recipe })
+        }
+    }
+
+    render() {
+        const { redirect, recipe, error } = this.state
+
+        if (redirect) {
+            return <Navigate to='/' />
+        } else {
+            return (
+                <div className="recipeForm__container">
+                    <textarea value={recipe.id} />
+                    <textarea value={recipe.name} />
+                    <textarea value={error} />
+                    <button onClick={() => this.save()}>Save</button>
+                </div>
+
+            )
+        }
+    }
 }
---
+++ Code/recipemanager/src/models/Recipe.js
@@ -0,0 +1,6 @@
+export const RecipesUrl = "/api/recipes"
+
+export const createNewRecipe = () => ({
+    id: '123',
+    name: 'Test'
+})
