f9b23d5b85b06908b0ba5dc7651e7fe2583bef96
Adrian <adrian.zigerlig@ost.ch>
2022-05-17 10:04:43 +0200

    REC-126: Added Recipe model test and structured RecipeEntry test

--- Code/recipemanager/src/tests/components/widgets/RecipeEntry.test.tsx
+++ Code/recipemanager/src/tests/components/widgets/RecipeEntry.test.tsx
@@ -6,6 +6,7 @@ test('renders recipe name field', () => {
     const testRecipe = getTestRecipe();
 
     render(<RecipeEntry recipe={testRecipe} />)
+    
     const linkElement = screen.getByText(/Testrezept/)
     expect(linkElement).toBeInTheDocument()
 });
@@ -14,6 +15,7 @@ test('renders recipe id field', () => {
     const testRecipe = getTestRecipe();
 
     render(<RecipeEntry recipe={testRecipe} />)
+
     const linkElement = screen.getByText(/5/)
     expect(linkElement).toBeInTheDocument()
 });
@@ -22,6 +24,7 @@ test('renders recipe name title', () => {
     const testRecipe = getTestRecipe();
 
     render(<RecipeEntry recipe={testRecipe} />)
+
     const linkElement = screen.getByText(/Rezept-Name/)
     expect(linkElement).toBeInTheDocument()
 });
@@ -30,13 +33,16 @@ test('renders recipe id title', () => {
     const testRecipe = getTestRecipe();
 
     render(<RecipeEntry recipe={testRecipe} />)
+
     const linkElement = screen.getByText(/Rezept-Id/)
     expect(linkElement).toBeInTheDocument()
 });
 
 function getTestRecipe() {
     const testRecipe = new Recipe();
+
     testRecipe.id = 5;
     testRecipe.name = "Testrezept";
+
     return testRecipe;
 }
