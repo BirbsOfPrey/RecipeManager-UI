4083df56e0a587d6f0de6ecd200ded2ce9774792
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-05-27 13:41:38 +0200

    REC-122: Change url from https to simple http setup. Change docker port and volume mapping.

--- Code/recipemanager/src/api.ts
+++ Code/recipemanager/src/api.ts
@@ -1,3 +1,3 @@
-export const RecipeManagerAPIEndpoint: string = 'https://localhost:13514/api'
+export const RecipeManagerAPIEndpoint: string = 'http://sinv-56060.rj.ost.ch/api'
 
 export const RecipesUrl: string = `${RecipeManagerAPIEndpoint}/recipe`
