bce3c818e5af1b0ee22bf5845d66c4a61e8d45e7
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-07-10 10:20:42 +0200

    REC-205: Remove old Frontend folder

--- Code/recipemanager-react/src/components/widgets/Copyright.tsx
+++
@@ -1,4 +0,0 @@
-import StringResource from '../../resources/StringResource'
-import './Copyright.css'
-
-export const Copyright = () => <div className="copyright__container">{StringResource.Copyright}</div>
--- Code/recipemanager-react/src/resources/Api.ts
+++
@@ -1,3 +0,0 @@
-export const RecipeManagerAPIEndpoint: string = 'http://sinv-56060.rj.ost.ch/api'
-
-export const RecipesUrl: string = `${RecipeManagerAPIEndpoint}/recipe`
