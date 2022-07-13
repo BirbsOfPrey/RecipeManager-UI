4baab91dedb346ccc30fdd1507305aa5345d7966
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-07-13 11:56:47 +0200

    REC-220: Update model classes

---
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/models/IngredientComponent.ts
@@ -0,0 +1,8 @@
+import { Ingredient } from "./Ingredient"
+
+export class IngredientComponent {
+    id?: number
+    amount?: number
+    physicalQuantity?: string
+    ingredient?: Ingredient
+}
--- Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/models/Recipe.ts
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/models/Recipe.ts
@@ -1,5 +1,9 @@
+import { IngredientComponent } from "./IngredientComponent"
+import { Step } from "./Step"
 
 export class Recipe{
     id?: number
     name?: string
+    steps?: Step[]
+    ingredientComponents?: IngredientComponent[]
 }
