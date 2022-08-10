fbade0703ee2e48a7f5e8bf5f077d16eb6a2438d
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-08-10 18:13:30 +0200

    REC-244: Load ingredients for Autofill

--- Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/controls/IngredientSelectCreate.tsx
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/controls/IngredientSelectCreate.tsx
@@ -1,7 +1,8 @@
-import { TextField, Autocomplete, Checkbox, Switch } from "@mui/material"
+import { TextField, Autocomplete, Switch } from "@mui/material"
 import { Component, ReactNode } from "react"
 import { Ingredient } from "../../models/Ingredient"
 import { IngredientValidators } from "../../models/IngredientValidators"
+import { createDefaultHeader, IngredientsUrl } from "../../resources/Api"
 import StringResource from "../../resources/StringResource"
 
 interface IProps {
@@ -11,17 +12,36 @@ interface IProps {
 
 interface IState {
     newIngredient: boolean
+    ingredientNames: string[]
 }
 
 export class IngredientSelectCreate extends Component<IProps, IState> {
     state: IState = {
-        newIngredient: false
+        newIngredient: false,
+        ingredientNames: []
     }
 
     changeMode = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
         this.setState({newIngredient: checked})
     }
 
+    async componentDidMount() {
+        await this.fetchIngredients()
+    }
+
+    fetchIngredients = async () => {
+        const response = await fetch(`${IngredientsUrl}`, {
+            headers: createDefaultHeader()
+        })
+        if (response.status >= 300) {
+            this.setState({ingredientNames: []})
+        } else {
+            const ingredients: Ingredient[] = await response.json()
+            const ingredientNames: string[] = ingredients.map(i => i.name ? i.name : "")
+            this.setState({ingredientNames: ingredientNames})
+        }
+    }
+    
     render() {
         let field: ReactNode
         if (this.state.newIngredient) {
@@ -42,7 +62,9 @@ export class IngredientSelectCreate extends Component<IProps, IState> {
                 <Autocomplete
                     disablePortal
                     className="ingredientSelectCreate__ingredient"
-                    options={["Hoi", "Hallo", "Velo"]}
+                    options={this.state.ingredientNames}
+                    value={this.props.ingredient ?  this.props.ingredient.name : ""}
+                    onChange={(_, value) => this.props.setValue(value ? value : "")}
                     renderInput={(params) => { return (<TextField {...params} fullWidth label={StringResource.General.Ingredient} variant="filled" />) }}
                 />)
         }
