37328004a5f7e9849ce41bc4946142859bf1bdc6
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-08-21 13:39:40 +0200

    REC-242: Add MUI theming

--- Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/App.tsx
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/App.tsx
@@ -6,25 +6,42 @@ import { Layout } from './Layout'
 import { RecipeManagement } from './pages/RecipeManagement'
 import { WeeklyScheduleView } from './pages/WeeklyScheduleView'
 import StringResource from '../resources/StringResource'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme, ThemeOptions } from '@mui/material/styles'
 import './App.css'
 
+const themeOptions: ThemeOptions = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#4caf50',
+        },
+        secondary: {
+            main: '#78909c',
+        },
+    },
+}
+
 export class App extends Component {
+
 
     render() {
         return (
             <div className={"app__container "}>
-                <Routes>
-                    <Route path={StringResource.Routes.Root} element={<Layout />}>
-                        <Route
-                            index
-                            element={<MainView />}
-                        />
-                        <Route path={`${StringResource.Routes.RecipeManagement}/*`} element={<RecipeManagement />} />
-                        <Route path={`${StringResource.Routes.WeeklySchedule}/*`} element={<WeeklyScheduleView />} />
-                        <Route path="user-session" element={<UserSession />} />
-                    </Route>
-                    <Route path={StringResource.Routes.Any} element={<p>{StringResource.Messages.NoContent}</p>} />
-                </Routes>
+                <ThemeProvider theme={createTheme(themeOptions)}>
+                    <Routes>
+                        <Route path={StringResource.Routes.Root} element={<Layout />}>
+                            <Route
+                                index
+                                element={<MainView />}
+                            />
+                            <Route path={`${StringResource.Routes.RecipeManagement}/*`} element={<RecipeManagement />} />
+                            <Route path={`${StringResource.Routes.WeeklySchedule}/*`} element={<WeeklyScheduleView />} />
+                            <Route path="user-session" element={<UserSession />} />
+                        </Route>
+                        <Route path={StringResource.Routes.Any} element={<p>{StringResource.Messages.NoContent}</p>} />
+                    </Routes>
+                </ ThemeProvider>
             </div>
         )
     }
