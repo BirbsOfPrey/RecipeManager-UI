60072139eddf1278e0e1e1de56c89ffae0df9adc
Adrian <adrian.zigerlig@ost.ch>
2022-08-24 13:17:11 +0200

    REC-256: Open recipe if clicked open button on ScheduledRecipe

--- Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/widgets/ScheduledRecipeListItem.tsx
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/components/widgets/ScheduledRecipeListItem.tsx
@@ -1,7 +1,9 @@
 import { IconButton, ListItemAvatar, ListItemText, ListItemSecondaryAction, ListItem } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete'
 import EventIcon from '@mui/icons-material/Event'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import { Component } from "react"
+import { Link } from 'react-router-dom'
 import { ScheduledRecipe } from "../../models/ScheduledRecipe"
 import StringResource from "../../resources/StringResource"
 
@@ -13,6 +15,8 @@ interface IProps {
 export class ScheduledRecipeListItem extends Component<IProps, {}> {
 
     render() {
+        const recipeRoute: string = `../${StringResource.Routes.RecipeManagement}/${StringResource.Routes.Recipe}/${this.props.scheduledRecipe.recipe?.id}`
+
         return (
             <ListItem
                 alignItems="flex-start"
@@ -26,12 +30,18 @@ export class ScheduledRecipeListItem extends Component<IProps, {}> {
                     secondary={this.props.scheduledRecipe.recipe?.description ? this.props.scheduledRecipe.recipe.description : StringResource.General.NoRecipeDescription}
                 />
                 <ListItemSecondaryAction>
-                        <IconButton
-                            edge="end"
-                            aria-label="delete"
-                            onClick={() => this.props.scheduledRecipeDeleted(this.props.scheduledRecipe.id)}>
-                            <DeleteIcon />
-                        </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="open"
+                        component={Link} to={`${recipeRoute}`}>
+                        <OpenInNewIcon />
+                    </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => this.props.scheduledRecipeDeleted(this.props.scheduledRecipe.id)}>
+                        <DeleteIcon />
+                    </IconButton>
                 </ListItemSecondaryAction>
             </ListItem>
         )
