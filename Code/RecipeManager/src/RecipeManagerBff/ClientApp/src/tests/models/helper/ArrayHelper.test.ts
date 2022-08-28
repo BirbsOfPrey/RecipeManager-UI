56683e960f277189d62a999ec76db15a2a3783c5
Adrian <adrian.zigerlig@ost.ch>
2022-08-28 14:23:10 +0200

    REC-239: Added ingredient and arrayHelper tests

---
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/tests/models/helper/ArrayHelper.test.ts
@@ -0,0 +1,11 @@
+import { NO_INDEX } from "../../../models/helper/ArrayHelper"
+
+test('NO-INDEX returns -1', () => {
+    // Arrange
+
+    // Act
+    const noIndex: number = NO_INDEX
+
+    // Assert
+    expect(noIndex).toBe(-1)
+})
