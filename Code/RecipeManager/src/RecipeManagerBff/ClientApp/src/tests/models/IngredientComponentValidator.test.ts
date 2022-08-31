5ac55de0456084174ff705753ba5bafa59e59671
Adrian <adrian.zigerlig@ost.ch>
2022-08-31 10:12:06 +0200

    REC-239: Adapted IngredientComponent validation

--- Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/tests/models/IngredientComponentValidator.test.ts
+++ Code/RecipeManager/src/RecipeManagerBff/ClientApp/src/tests/models/IngredientComponentValidator.test.ts
@@ -3,7 +3,7 @@ import { IngredientComponentValidator } from "../../models/IngredientComponentVa
 describe('Method validateAmount returns ', () => {
     it.each([
         [true, 15],
-        [false, 0],
+        [true, 0],
         [false, -1],
         [false, undefined]
     ])('%p when invoked with amount: %p', (result: boolean, amount?: number) => {
