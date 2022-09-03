37735647bf58788bc98b7e786900d99037ee206f
BirbsOfPrey <60753183+BirbsOfPrey@users.noreply.github.com>
2022-09-03 16:05:36 +0200

    REC-135: Introduce application configration for BFF

---
+++ Code/RecipeManager/src/RecipeManagerBff/Settings/ApiSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManagerBff.Settings;
+
+public class ApiSettings : IApiSettings
+{
+  [Required] 
+  public string Authority { get; set; } = null!;
+  [Required]
+  public string ApiScopeName { get; set; } = null!;
+  [Required]
+  public string BffClientId { get; set; } = null!;
+  [Required]
+  public string BffClientSecret { get; set; } = null!;
+  [Required]
+  public string ProxyRoute { get; set; } = null!;
+  [Required]
+  public string ProxyAddress { get; set; } = null!;
+}
